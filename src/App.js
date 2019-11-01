1b88c15e7a7c220799f908b600dee6671e6b5292
arthur67 <arthurv67@gmail.com>
2019-11-01 15:50:13 +1100

    button color change code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Tweet from './Tweet.js';
 
 function App() {
-   return (
+  const [isRed, setRed] = useState(false);
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
+
+  return (
     <div className="app">
-      <Tweet name="Terminator" message="I'll be back" />
+    <h1 className={isRed ? "red" : ""}>Change the color</h1>
+    <button onClick={increment}>Increment</button>
+    <h1>{count}</h1>
+
+      {/* <Tweet name="Terminator" message="I'll be back" />
       <Tweet name="Incinerator" message="Burn" />
       <Tweet name="Rambo" message="Why you pushn me?" />
-      <Tweet name="Dirty Harry" message="Make my day!" />
+      <Tweet name="Dirty Harry" message="Make my day!" /> */}
     </div>
   );
 }
